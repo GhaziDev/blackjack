fec2e2c13383029db59c7f17f7019f246e52b770
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2019-11-09 17:22:30 +0530

    Update project22.py

--- project22.py
+++ project22.py
@@ -1,3 +1,4 @@
+# A Basic implementation of Card game named BlackJack .. 
 import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
